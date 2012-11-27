21c8f9d24829f0b6dc5d9c98e787b9708386700e
Lloyd Emelle <lloyd@emelle.me>
2012-11-27 14:19:21 +0000

    Adding files to github

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,16 +3,25 @@
 global $project;
 $project = 'mysite';
 
-global $database;
-$database = '';
-
-require_once('conf/ConfigureFromEnv.php');
+global $databaseConfig;
+$databaseConfig = array(
+	"type" => 'MySQLDatabase',
+	"server" => 'localhost',
+	"username" => 'root',
+	"password" => 'd3v-Emelle',
+	"database" => 'SS_interactiveguide',
+	"path" => '',
+);
 
 MySQLDatabase::set_connection_charset('utf8');
 
-// Set the current theme. More themes can be downloaded from
-// http://www.silverstripe.org/themes/
-SSViewer::set_theme('simple');
+
+Director::addRules(50, array('operator-guide/$Action/$ID/$Name' =>'AdminPage_Controller'));
+Director::addRules(50, array('operators/$ID/$Action/$Name' =>'OperatorsPage_Controller'));
+// Set the site locale
+i18n::set_locale('en_US');
 
 // Enable nested URLs for this site (e.g. page/sub-page/)
-if(class_exists('SiteTree')) SiteTree::enable_nested_urls();
+if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
+
+DMSDocumentAddController::add_allowed_extensions(array('psd'));
